1d47e27f44b4c30acce0a293ecb3dd0ebecc8cf3
Venkat.R <ramsunvtech@gmail.com>
2023-12-17 21:58:44 +0800

    tech(base): add IF Statements

---
+++ c-if.py
@@ -0,0 +1,26 @@
+# Statement
+print('\n >>>> Statement Example');
+
+# isEligibleToVote Function
+print('\n >>>> isEligibleToVote Function with passing age');
+
+def isEligibleToVote(age = 0):
+    if(age < 21):
+        print(str(age) + ' Wow, You will be soon eligible to vote');
+    elif(age > 21):
+        print(str(age) + ' Congrats, You are eligible to vote!');
+    else:
+        print(str(age) + ' Invalid `age` parameter');
+
+
+# Example usage
+print('\n >>>> Passing nothing consider default parameter if provided');
+isEligibleToVote()
+print('\n >>>> Passing `16` return "Wow, You will be soon eligible to vote"');
+isEligibleToVote(16)
+print('\n >>>> Passing `19` return "Wow, You will be soon eligible to vote"');
+isEligibleToVote(19)  
+print('\n >>>> Passing `25` return "Congrats, You are eligible to vote!"');
+isEligibleToVote(25)  
+print('\n >>>> Passing `\'\'` as parameter throws TypeError: \'<\' not supported between instances of \'str\' and \'int\'');
+# isEligibleToVote('')  
