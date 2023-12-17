68f63df3568690958e1fe683250b8bdb693d6cab
Venkat.R <ramsunvtech@gmail.com>
2023-12-17 23:29:20 +0800

    tech(data-type): update 15 data types and 7 set

--- d-data-types.py
+++ d-data-types.py
@@ -1,6 +1,21 @@
-print("Variable name should start with alpha-numeric characters, _ or string");
-print("Variable name should not start with numbers, hyphen");
-print("Variable names are case sensitive");
+print('\n >>>> Data Type Example');
+print('15 Types of Data Types');
+print('>>>> Text Type');
+print('1. string\n')
+print('>>>> Numeric Type');
+print('2. int, 3. float, 4. complex\n')
+print('>>>> Sequence Type');
+print('5. list, 6. tuple, 7. range\n')
+print('>>>> Mapping Type');
+print('8. dict\n')
+print('>>>> Set Type');
+print('9. set, 10. frozenset\n')
+print('>>>> Boolean Type');
+print('11. bool\n')
+print('>>>> Binary Type');
+print('11. bytes, 12. byteArray, 13. memoryView\n')
+print('>>>> None Type');
+print('11. None\n')
 
 greet = 'welcome' # String
 castToString = str(3)
@@ -18,7 +33,23 @@ print(f"\n>>> Type of weight is {type(weight)}")
 castToFloat = float('75.50')
 print(f">>> Type of castToFloat is {type(castToFloat)}")
 
+print("\n>>> List Type")
 fruits = ["apple", "banana", "cherry"] # List for fruits
-print(f"\n>>> Type of fruits is {type(fruits)}")
-# castToFloat = float('75.50')
-# print(f">>> Type of castToFloat is {type(castToFloat)}")
+print(f">>> Type of fruits is {type(fruits)}")
+fruits.append("grapes");
+print('Fruits List: ', fruits);
+
+print("\n>>> Tuple Type")
+configuration = ('2MB','30 Minutes') # Tuple 
+print(f">>> Type of configuration is {type(configuration)}")
+print('configuration Tuple: ', configuration);
+
+print("\n>>> Dict Type")
+employeeDictionary = {"name" : "John", "age" : 36} # Dict
+print(f">>> Type of employeeDictionary is {type(employeeDictionary)}")
+print('employee dictionary: ', employeeDictionary);
+
+print("\n>>> Boolean Type")
+eligibleToVote = True # Boolean
+print(f">>> Type of eligibleToVote is {type(eligibleToVote)}")
+print('eligibleToVote: ', eligibleToVote);
