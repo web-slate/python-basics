721f20c528af1d1a0bee827bc36d16ee128e142f
Venkat.R <ramsunvtech@gmail.com>
2023-12-19 00:27:33 +0800

    tech(numbers): add number data type examples

---
+++ f-numbers.py
@@ -0,0 +1,19 @@
+print('\n >>>> Number Data Type Example')
+
+print('\n >>>> Integer Data Type Example')
+age = 25
+print(f'Int age ({age}) can be whole number, positive or negative, without decimals, of unlimited length.')
+print('examples: 35656222554887711, -3255522, 25')
+
+print('\n >>>> Float Data Type Example')
+weight = 76.50
+print(f'Float weight ({weight}) can be number, positive or negative, containing one or more decimals')
+print('examples: 1.10, 1.0, -35.59, 35e3, 12E4, -87.7e100')
+
+print('\n >>>> Complex Data Type Example')
+something = 1j
+print(f'complex something ({something}) are written with a "j" as the imaginary part')
+print('examples: 1.10, 1.0, -35.59, 35e3, 12E4, -87.7e100')
+ 
+print('\n >>>> Type Case Functions');
+print('examples: float(weight), int(age), complex()');
