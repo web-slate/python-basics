ddcb44f0003484e5d3fdd6fe298c3333833fdfe4
Venkat.R <ramsunvtech@gmail.com>
2023-12-19 09:08:51 +0800

    tech(operators): add all possible operators

---
+++ h-operators.py
@@ -0,0 +1,22 @@
+print('\n >>>> Operators Example')
+print('Arithmetic operators: (+,-,*,/,//,%,**)')
+print('Assignment operators: (=,+=,-=,*=,/=,//=,**=,\=,^=,>>=,<<=)')
+print('Comparison operators: (==,!=,>,>=,<,<=)')
+print('Logical operators: (and ,or ,not)')
+print('Membership operators: (in ,not in)')
+print('Identity operators: (is, is not)')
+print('Bitwise Operators: (&,|,^,~,<<,>>)')
+
+print('\n Arithmetic Interesting Operators')
+print (' >>> Exponentiation Operator')
+x = 2
+y = 5
+print('when x is 2 and y = 5')
+print('Exponentiation: x ** y is 2 to power of 5 aka 2^5 = ', x**y)
+
+print ('\n >>> Floor Division Operator')
+fruits = 10
+share_people = 3
+print('when fruits are 10 and people are 3 and we need to share the fruits')
+print('then we can use Floor Division ')
+print('Maximum can be shared: fruits // share_people = ', fruits // share_people)
