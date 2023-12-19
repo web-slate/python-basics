28c97d201bb71f3227f9e5ddbbf725a46291a9c7
Venkat.R <ramsunvtech@gmail.com>
2023-12-19 20:07:43 +0800

    tech(problems): update test case number length

--- problems/number_length.py
+++ problems/number_length.py
@@ -1,5 +1,6 @@
 print('\n >>> Number Length Example without using len() def')
 print(' >>> number_length()')
+
 def number_length(number):
     if number == 0:
         return 1
@@ -11,11 +12,32 @@ def number_length(number):
     
     return count;
 
+failures = None
 
-assert number_length(0) == 1
-assert number_length(25) == 2
-assert number_length(725) == 3
-assert number_length(4325) == 4
-assert number_length(734250) == 6
-assert number_length(52734250) == 8
-print("Number Length Example tests are passed!")
+def flag_failure():
+    global failures
+    failures = (failures or 0) + 1
+    
+def failure_count():
+    return failures
+
+def print_and_assert(param, expected):
+    try:
+        assert number_length(param) == expected
+        print(f'✅ Pass: number_length({param}) is returning {expected}')
+    except AssertionError:
+        flag_failure()
+        print(f'❌ AssertionError: number_length({param}) is returning {expected}')
+
+print('\n >>> Number Length Test Cases')
+print_and_assert(0, 2)
+print_and_assert(25, 2)
+print_and_assert(725, 3)
+print_and_assert(4325, 4)
+print_and_assert(734250, 6)
+print_and_assert(52734250, 8)
+
+if (failure_count() > 0):
+    print(f"{failure_count()} Failure in Number Length Example tests")
+else:
+    print("Number Length Example tests are passed!")
