1cac95475d65f11ddf66696208b73b7e23a05ebe
Venkat.R <ramsunvtech@gmail.com>
2023-12-19 21:15:50 +0800

    tech(problems): add string length function

--- problems/string_length.py
+++ problems/string_length.py
@@ -1,16 +1,19 @@
+from testUtils import print_and_assert, getTestResult
+
 print('\n >>> String Length Example without using len() def')
 print(' >>> string_length()')
 def string_length(text):
     count = 0;
     for character in text:
         count += 1
-    
     return count;
 
+print('\n >>> String Length Test Cases')
+print_and_assert(string_length, 'Venkat', 6)
+print_and_assert(string_length, 'R.Venkat', 8)
+print_and_assert(string_length, 'Python', 6)
+print_and_assert(string_length, 'Full Stack Development', 10)
+print_and_assert(string_length, '734250', 6)
+print_and_assert(string_length, '52734250', 9);
 
-print("string_length('Venkat'): ", string_length('Venkat'))
-print("string_length(R.Venkat): ", string_length('R.Venkat'))
-print("string_length('Python'): ", string_length('Python'))
-print("string_length('Full Stack Development'): ", string_length('Full Stack Development'))
-print("string_length(734250): ", string_length(734250))
-print("string_length(52734250): ", string_length(52734250))
+getTestResult('String Length')
