542230e89c1c13e0c0d49a8e269e6406bdf5ea66
Venkat.R <ramsunvtech@gmail.com>
2023-12-19 21:10:53 +0800

    tech(errors): add additinal error message

--- errors.py
+++ errors.py
@@ -1,1 +1,7 @@
-# SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+print('\nSyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers')
+
+print("\nModuleNotFoundError: No module named 'utils'")
+print("soluton: \nimport sys \n sys.path.append('../')")
+
+print("\n ImportError: cannot import name 'failure_count' from 'testUtils'")
+print("Soluton: Actual issue, function was missing")
