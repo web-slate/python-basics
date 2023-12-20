65d14232a7105dd5f904e5fa4a0aabb0ba3c9a2f
Venkat.R <ramsunvtech@gmail.com>
2023-12-20 09:53:34 +0800

    tech(defs): add even sequence from list

---
+++ defs/print_even_sequence.py
@@ -0,0 +1,12 @@
+def print_even_sequence(array):
+  for a in array:
+    if a % 2 == 0:
+      print("\nSequence for {0}".format(a))
+      for k in range(0,a):
+        print(k, end=" ")
+        
+print_even_sequence([10, 22, 33, 44, 55, 66, 77])
+print('\n Here O(a * k) , where a = size of array and k is largest even')
+
+print('\n\n 🕒 Time Complexity: Approximately O(n × m) \n In the worst case, where n is the length of the array and m is the average size of the even numbers. However, the actual time complexity can vary significantly based on the contents of the input array.')
+print('\n 💾 Space Complexity: O(1) \n As it only uses a constant amount of additional memory.')
