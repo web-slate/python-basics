a195bfddce7ceb7c88f3f91ebea58ca6af781e01
Venkat.R <ramsunvtech@gmail.com>
2023-12-20 09:54:00 +0800

    tech(range): add further print logs

--- range.py
+++ range.py
@@ -1,17 +1,20 @@
 print('\n >>>> Range Data Type Example')
-print('\n >>>> Print Until 7')
+print('\n >>>> Print Until 7 `range(8)` wrap with list')
 print(list(range(8)))
 
-print('\n >>>> Print from 2 to 7')
+print('\n >>>> Print from 2 to 7 `range(2, 8)` wrap with list')
 print(list(range(2, 8)))
 
 print('\n >>>> Print from 0 to 20 increment 2 like 2 Table')
+print('Using `range(0, 13, 2)` wrap with list')
 print(list(range(0, 13, 2)))
 
 print('\n >>>> Print from 0 to 20 increment 2 like 4 Table')
+print('Using `range(0, 13, 4)` wrap with list')
 print(list(range(0, 13, 4)))
 
 print('\n >>>> Print from 0 to 20 increment 2 like 7 Table')
+print('Using `range(0, 13, 7)` wrap with list')
 print(list(range(0, 13, 7)))
 
 print('\n Type value of type(range(2, 8)) is below')
