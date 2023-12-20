0515d3dc032e9b046653d1c2968fd9e1890207a7
Venkat.R <ramsunvtech@gmail.com>
2023-12-20 22:11:01 +0800

    tech(base): add sequence data types

---
+++ sequence-data-types.py
@@ -0,0 +1,8 @@
+print('\n >>>> Sequence Data Type Example')
+
+print('\n >>>> List Data Type Example')
+print(' >>> Create N number of List element with same value')
+print([7] * 4);
+
+print('\n >>>> Tuple Data Type Example')
+print('\n >>>> Complex Data Type Example')
