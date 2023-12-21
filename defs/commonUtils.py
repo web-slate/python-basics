4f7b14a11841075c9cd6b80dd604f906182d3ff5
Venkat.R <ramsunvtech@gmail.com>
2023-12-22 06:32:58 +0800

    tech(def/utils): updated commo and test utils

--- defs/commonUtils.py
+++ defs/commonUtils.py
@@ -2,12 +2,12 @@ def timeComplexity(value, desc):
     result = f'\n 🕒 Time Complexity: Approximately {value}'
 
     if (desc):
-        result += f'\n     Description: {desc}'
+        result += f'\n    Description: {desc}'
     print(result)
 
 def spaceComplexity(value, desc):
     result = f' 💾 Space Complexity: Approximately {value}'
 
     if (desc):
-        result += f'\n     Description: {desc}'
+        result += f'\n    Description: {desc}'
     print(result)
--- defs/testUtils.py
+++ defs/testUtils.py
@@ -7,6 +7,11 @@ def flag_failure():
 def failure_count():
     return failures
 
+def solution_title(title):
+    print('=' * 70)
+    print(f'>>> {title}')
+    print('=' * 70)
+
 def print_and_assert(function_name, param, expected):
     try:
         assert function_name(param) == expected
@@ -14,7 +19,15 @@ def print_and_assert(function_name, param, expected):
     except AssertionError:
         flag_failure()
         print(f'❌ AssertionError: {function_name.__name__}({param}) is returning {function_name(param)} but `{expected}` is expected')
-        
+
+def print_and_assert_new(function_name, *params, expected):
+    try:
+        assert function_name(*params) == expected
+        print(f'✅ Pass: {function_name.__name__}{params} is returning {expected} as expected')
+    except AssertionError:
+        flag_failure()
+        print(f'❌ AssertionError: {function_name.__name__}{params} is returning {function_name(*params)} but `{expected}` is expected')
+     
 def getTestResult(testName):
     if (failure_count() > 0):
         print(f"{failure_count()} Failure in {testName} tests")
