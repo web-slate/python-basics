987c4cf17e3417cc234dba002b9ebf9f6ac7e030
Venkat.R <ramsunvtech@gmail.com>
2023-12-21 19:33:28 +0800

    tech(util): add common util for time and space complexity print

--- defs/commonUtil.py
+++
@@ -1,6 +0,0 @@
-def timeComplexity(value, desc):
-    print(f'\n\n 🕒 Time Complexity: Approximately {value} \n {desc}.')
-
-def spaceComplexity(value, desc):
-    print(f'\n 💾 Space Complexity: Approximately {value} \n {desc}.')
-
