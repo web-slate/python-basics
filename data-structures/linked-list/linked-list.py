757dad93a6b40c43796f4c4a39fc95bd0f930c6b
Venkat.R <ramsunvtech@gmail.com>
2023-12-22 08:55:11 +0800

    linked-list(): add with and without tail solution

--- data-structures/linked-list/linked-list.py
+++ data-structures/linked-list/linked-list.py
@@ -4,13 +4,29 @@ from commonUtils import timeComplexity, spaceComplexity
 print('\n >>> Linked List Implementation')
 print('>>> 1. Create Node Class with 2 properties 1. data and 2. next property')
 print('>>> 2. Create LinkedList Class with property called head')
+print('''
+      Sample Representation of Data
+        node(
+            data=1,
+            next=node(
+                data=2,
+                next=node(
+                    data=3,
+                    next=node(
+                        data=4,
+                        next=None
+                    )
+                )
+            )
+        )
+''')
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-class LinkedList:
+class LinkedListWithoutTail:
     def __init__(self):
         self.head = None
 
@@ -33,10 +49,43 @@ class LinkedList:
             cur_node = cur_node.next
         print("None")
 
-# Example Usage
-linked_list = LinkedList()
+class LinkedListWithTail:
+    def __init__(self):
+        self.head = None
+        self.tail = None  # Maintaining a reference to the last node
+
+    def append(self, data):
+        """ Append a node to the end of the list in O(1) """
+        new_node = Node(data)
+        if not self.head:  # If the list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node  # Update the tail reference
+
+    def print_list(self):
+        """ Print all elements of the list """
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+new_node = Node(1)
+
+solution_title('Linked List without Tail')
+linked_list = LinkedListWithoutTail()
 linked_list.append(1)
 linked_list.append(2)
 linked_list.append(3)
 
 linked_list.print_list()  # Outputs: 1 -> 2 -> 3 -> None
+
+solution_title('Linked List with Tail')
+linked_list_2 = LinkedListWithoutTail()
+linked_list_2.append(1)
+linked_list_2.append(2)
+linked_list_2.append(3)
+
+linked_list_2.print_list()  # Outputs: 1 -> 2 -> 3 -> None
