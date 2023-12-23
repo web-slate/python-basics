05b874a1a3ba3f71d4ca356b8ffdea19f7d5883d
Venkat.R <ramsunvtech@gmail.com>
2023-12-23 20:41:09 +0800

    problems(palindrome): add is_palindrome solutions

---
+++ problems/palindrome.py
@@ -0,0 +1,36 @@
+from testUtils import print_and_assert, getTestResult, solution_title
+
+print('\n >>> Is Palindrome Example')
+print('\n >>> The concept of a palindrome focuses solely on the sequence of characters, disregarding spaces and letter casing.')
+print(' >>> palindrome(word)')
+
+class palindromSolutions(object):
+    def using_string_solution(self, word):
+        return word[::-1]
+
+    def using_iteration_with_range(self, word):
+        last_index = len(word) - 1
+        text = ''
+        for i in range(last_index, -1, -1):
+            text += word[i]
+        return text
+
+palindrome = palindromSolutions()
+using_string_solution = palindrome.using_string_solution
+solution_title('Using String Slicing')
+print_and_assert(using_string_solution, 'madam', 'madam')
+print_and_assert(using_string_solution, 'kayak', 'kayak')
+# print_and_assert(using_string_solution, 'EVIL OLIVE', 'EVIL OLIVE')
+# print_and_assert(using_string_solution, 'DO GEESE SEE GOD', 'DO GEESE SEE GOD')
+
+getTestResult('Simple Palindrome')
+
+# using_iteration_with_range = palindrome.using_iteration_with_range
+# solution_title('Using Iteration with Range')
+# print_and_assert(using_iteration_with_range, 'madam', 'madam')
+# print_and_assert(using_string_solution, 'kayak', 'kayak')
+# print_and_assert(using_string_solution, 'EVIL OLIVE', 'EVIL OLIVE')
+# # print_and_assert(using_string_solution, 'DO GEESE SEE GOD', 'DO GEESE SEE GOD')
+
+# getTestResult('Using Iteration with Range')
+
