5f76bfe8bb10a3acb22d7214cda2f9a5d29609bd
Venkat.R <ramsunvtech@gmail.com>
2023-12-25 08:18:17 +0800

    data-type(string): add slicing example

---
+++ data-types/string/slicing.py
@@ -0,0 +1,12 @@
+print('\n >>>> String Slicing Example')
+
+greet = "welcome"
+debit_card = "1234-5678-9012-3456"
+slicing_detailed_desc = '''
+In Python, slicing a string, list, or any other sequence type is a very efficient operation in terms of time complexity. Here's a brief overview:
+Time Complexity: O(k)
+Space Complexity:: O(k)
+'''
+
+print('\n >>>> Get last four digit of debit card')
+print(' ', debit_card[-4:])
