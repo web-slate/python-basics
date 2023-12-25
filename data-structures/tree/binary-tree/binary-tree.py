98a04b047daa7db67950346c93dc6721404c07f5
Venkat.R <ramsunvtech@gmail.com>
2023-12-25 10:29:16 +0800

    data-strcuture(binary-tree): update basic notes

---
+++ data-structures/tree/binary-tree/binary-tree.py
@@ -0,0 +1,10 @@
+
+print('\n >>> Binary Tree Implementation')
+print('- each node has at most two children')
+print('- referred to as the left child and the right child.')
+print('- no specific ordering in terms of how nodes are arranged.')
+print('    A')
+print('   / \\')
+print('  B   C')
+print(' /   / \\')
+print('D   E   F')
