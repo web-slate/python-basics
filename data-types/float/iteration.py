fbb69845bd9a82c3aaea5bfeb2c92d8db43a57f8
Venkat.R <ramsunvtech@gmail.com>
2023-12-26 21:21:48 +0800

    data-types(iteration): add simple example with and with type casting

--- data-types/float/iteration.py
+++ data-types/float/iteration.py
@@ -5,38 +5,53 @@ weight = 68.50
 height = 6.3
 distance = 10.2
 price = 27.50
+latitude = 40.741895
+longitude = -73.989308
 
-# for i in range(1,11): 
-    # print(i)
-
-# i = 1
-# while i < 11:
-#     print(i)
-#     i +=1
-    
-# i = 10
-# while i > 0:
-#     print(i)
-#     i-=1
-
-print('\n >>>> Iterate using for loop with type casting')
+print('\n >>>> Iterate using for loop with type casting to string')
 print(' >>>> Follow this approach on demand and Its not recommended')
-for text in str(mark):
-    print(text)
+def iterateAfterTypeCasting(float_number):
+    formatted_number = "{:.2f}".format(float_number)
+    for text in formatted_number:
+        print(text)
 
-print('\n >>>> Iterate using for loop with operators')
-start = 0.0
-end = mark
-step = 0.1
+# iterateAfterTypeCasting(mark)
+# iterateAfterTypeCasting(weight)
+# iterateAfterTypeCasting(height)
+# iterateAfterTypeCasting(distance)
+# iterateAfterTypeCasting(price)
+print('\n >>>> Iterate using while loop with operators')
+number = 78.88
 
-current_value = start
-while current_value <= end:
-    print(current_value)
-    current_value += step
+# Handle the integer part
+
+# # Handle the fractional part
+# fractional_part = (number - int(number))
+# fractional_part = int((number - int(number)) * 100)
+# rounded_fractional_part = round(fractional_part, 2)
+# rounded_fractional_part_2 = int(fractional_part * 100 + 0.5) / 100.0
+
+# print('fractional_part', fractional_part)
+# print('rounded_fractional_part', rounded_fractional_part)
+# print('rounded_fractional_part_2', rounded_fractional_part_2)
+
+def iterateWithoutTypeCasting(float_number):
+    int_part = int(float_number)
+    stack = []
+    while int_part > 0:
+        digit = int_part % 10
+        stack.append(digit)
+        int_part //= 10
+    
+    while stack:
+        print(stack.pop())
+
+
+iterateWithoutTypeCasting(mark)
+# iterateWithoutTypeCasting(weight)
+# iterateWithoutTypeCasting(height)
+# iterateWithoutTypeCasting(distance)
+# iterateWithoutTypeCasting(price)
+# iterateWithoutTypeCasting(latitude)
 
 # print('\n >>>> Iterate using for loop with range')
-# start = 0.0
-# end = mark
-# step = 0.1
-
-# for i in range(int((end-start) / step) + 1):
