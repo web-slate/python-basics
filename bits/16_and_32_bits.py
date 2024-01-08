f9361ef49398b22a6f08ea52c859ea4176e64cab
Venkat.R <ramsunvtech@gmail.com>
2024-01-08 22:47:42 +0800

    bit: add 16_and_32_bits

---
+++ bits/16_and_32_bits.py
@@ -0,0 +1,33 @@
+import data_types.commonUtils as utils
+
+utils.print_h1('16 and 32 Bit Basics')
+
+utils.print_h2('16 Bits')
+print('Note: 16 bits can represent numbers from 0 to 65,535 (2^16 - 1).')
+print('''
+| Decimal | Binary            |
+|---------|-------------------|
+| 0       | 0000000000000000  |
+| 1       | 0000000000000001  |
+| 2       | 0000000000000010  |
+| 3       | 0000000000000011  |
+| 4       | 0000000000000100  |
+| 5       | 0000000000000101  |
+| 6       | 0000000000000110  |
+| 7       | 0000000000000111  |
+| 8       | 0000000000001000  |
+| 9       | 0000000000001001  |
+| 10      | 0000000000001010  |
+''')
+
+utils.print_h2('32 Bits')
+print('Note: 32 bits can represent numbers from 0 to 4,294,967,295 (2^32 - 1). Full binary representation for numbers larger than 16 bits is quite lengthy, so only the least significant 16 bits are shown for brevity.')
+print('''
+| Decimal | Binary (only showing least significant 16 bits for brevity) |
+|---------|------------------------------------------------------------|
+| 0       | 0000000000000000                                           |
+| 1       | 0000000000000001                                           |
+| 2       | 0000000000000010                                           |
+| ...     | ...                                                        |
+| 10      | 0000000000001010                                           |
+''')
