d139d2676d3d2765442108114ab1021996049afc
Venkat.R <ramsunvtech@gmail.com>
2024-02-27 06:27:02 +0800

    add returning multiple value

--- functions/simple_function.py
+++ functions/simple_function.py
@@ -1,1 +1,12 @@
-// simple function
+def returning_one():
+  return 1
+
+def returning_one_with_comma():
+  return 1,
+
+def returning_one_two_three():
+  return 1,2,3
+
+print(type(returning_one()), 'returning_one: ', returning_one())
+print(type(returning_one_with_comma()), 'returning_one_with_comma: ', returning_one_with_comma())
+print(type(returning_one_two_three()), 'returning_one_two_three: ', returning_one_two_three())
