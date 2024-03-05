bf6d20f57b6acd2c6070ebb98c9f2ac02c860f86
Venkat.R <ramsunvtech@gmail.com>
2024-03-05 20:41:49 +0800

    remove unneccassary semi colon

--- basics/c_if.py
+++ basics/c_if.py
@@ -1,25 +1,25 @@
 # Statement
-print('\n >>>> Statement Example');
+print('\n >>>> Statement Example')
 
 # isEligibleToVote Function
-print('\n >>>> isEligibleToVote Function with passing age');
+print('\n >>>> isEligibleToVote Function with passing age')
 
 def isEligibleToVote(age: int = 0) -> None:
     if(age < 21):
-        print(str(age) + ' Wow, You will be soon eligible to vote');
+        print(str(age) + ' Wow, You will be soon eligible to vote')
     elif(age > 21):
-        print(str(age) + ' Congrats, You are eligible to vote!');
+        print(str(age) + ' Congrats, You are eligible to vote!')
     else:
-        print(str(age) + ' Invalid `age` parameter');
+        print(str(age) + ' Invalid `age` parameter')
 
 # Example usage
-print('\n >>>> Passing nothing consider default parameter if provided');
+print('\n >>>> Passing nothing consider default parameter if provided')
 isEligibleToVote()
-print('\n >>>> Passing `16` return "Wow, You will be soon eligible to vote"');
+print('\n >>>> Passing `16` return "Wow, You will be soon eligible to vote"')
 isEligibleToVote(16)
-print('\n >>>> Passing `19` return "Wow, You will be soon eligible to vote"');
+print('\n >>>> Passing `19` return "Wow, You will be soon eligible to vote"')
 isEligibleToVote(19)  
-print('\n >>>> Passing `25` return "Congrats, You are eligible to vote!"');
+print('\n >>>> Passing `25` return "Congrats, You are eligible to vote!"')
 isEligibleToVote(25)  
-print('\n >>>> Passing `\'\'` as parameter throws TypeError: \'<\' not supported between instances of \'str\' and \'int\'');
-# isEligibleToVote('')
+print('\n >>>> Passing `\'\'` as parameter throws TypeError: \'<\' not supported between instances of \'str\' and \'int\'')
+isEligibleToVote('')
