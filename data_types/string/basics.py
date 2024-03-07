3fcb58eb3f50accf8011e99119a4eff6702df077
Venkat.R <ramsunvtech@gmail.com>
2024-03-07 17:10:34 +0800

    added substring approach tables and example implementation

--- data_types/string/basics.py
+++ data_types/string/basics.py
@@ -10,6 +10,7 @@ utils.print_ordered_list([
   f'Finding Length using len(str): "{language}" is , {len(language)}',
   f'Middle character index of "{language}" is {mid_index} and char is {language[mid_index]} Using ',
   f'6 different ways of String concatenations (+, %, join, format, fstring, += Operator)'
+  f'6 different ways of String Substring (slicing, find, index, regEx, strip, partition)'
 ])
 
 greet = 'Hello'
@@ -37,3 +38,34 @@ utils.print_span('Using String format() Method: ', '{} {}'.format(greet, name))
 utils.print_span('Using f-String (Python 3.6+): ', f'{greet} {name}')
 greet += name
 utils.print_span('Using String Concatenation with +=: ', greet, '\n')
+
+
+substring_methods = [
+    (" Approach", " Example Code", " Time Complexity", " Space Complexity"),
+    (" Slicing", " 'Python'[1:4]", " O(k)", " O(k)"),
+    (" str.split() & Indexing", " 'a,'.split(',')[1]", " O(n)", " O(n)"),
+    (" str.find() or str.index() & Slicing", " s = 'P'; s[s.find('th'):]", " O(n)", " O(k)"),
+    (" Regular Expression (re module)", " re.search('y.*o', 'Python').group()", " O(n)", " O(k)"),
+    (" str.strip()/str.rstrip()/str.lstrip()", "' P '.strip()", " O(n)", " O(n)"),
+    (" str.partition() & Indexing", " 'P'.partition('-')[0]", " O(n)", " O(n)")
+]
+
+utils.print_tabular_list(substring_methods)
+
+
+utils.print_h6('We have greet variable hold "Hello" and name hold "Venkat.R"')
+word = 'An Apple'
+utils.print_span('Using Slicing: [start:stop:step]', word[3:])
+utils.print_span('Using Split Function returns list:', word.split(' ')[1])
+utils.print_span('Using find() return index, -1 when not found:', word[word.find(' A'):])
+utils.print_span('Using index() return index, ValueError when not found:', word[word.index(' A'):])
+import re
+utils.print_span('Using Regular Expression():', re.search('Ap.*e', word).group())
+
+
+def char_and_ascii(s):
+    return [(char, ord(char)) for char in s]
+
+# Example usage
+result = char_and_ascii("An Apple")
+print(result)
