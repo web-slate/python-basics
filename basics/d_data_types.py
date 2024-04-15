239731b1a329dab2c64ab8c4652bc80aa06cad82
Venkat.R <ramsunvtech@gmail.com>
2024-04-15 20:41:20 +0800

    update data types

--- basics/d_data_types.py
+++ basics/d_data_types.py
@@ -1,20 +1,20 @@
-print('\n >>>> Data Type Example');
-print('15 Types of Data Types');
-print('>>>> 1st. Text Type');
+print('\n >>>> Data Type Example')
+print('15 Types of Data Types')
+print('>>>> 1st. Text Type')
 print('1. string\n')
-print('>>>> 2nd Numeric Type');
+print('>>>> 2nd Numeric Type')
 print('2. int, 3. float, 4. complex\n')
-print('>>>> 3rd Sequence Type');
+print('>>>> 3rd Sequence Type')
 print('5. list, 6. tuple, 7. range\n')
-print('>>>> 4th Mapping Type');
+print('>>>> 4th Mapping Type')
 print('8. dict\n')
-print('>>>> 5th Set Type');
+print('>>>> 5th Set Type')
 print('9. set, 10. frozenset\n')
-print('>>>> 6th Boolean Type');
+print('>>>> 6th Boolean Type')
 print('11. bool\n')
-print('>>>> 7th Binary Type');
+print('>>>> 7th Binary Type')
 print('11. bytes, 12. byteArray, 13. memoryView\n')
-print('>>>> 8th None Type');
+print('>>>> 8th None Type')
 print('11. None\n')
 
 greet = 'welcome' # String
@@ -37,25 +37,25 @@ print(f">>> Type of castToFloat is {type(castToFloat)}")
 print("\n>>> List Type")
 fruits = ["apple", "banana", "cherry"] # List for fruits
 print(f">>> Type of fruits is {type(fruits)}")
-fruits.append("grapes");
-print('Fruits List: ', fruits);
+fruits.append("grapes")
+print('Fruits List: ', fruits)
 
 print("\n>>> Tuple Type")
 configuration = ('2MB','30 Minutes') # Tuple 
 print(f">>> Type of configuration is {type(configuration)}")
-print('configuration Tuple: ', configuration);
+print('configuration Tuple: ', configuration)
 
 print("\n>>> Dict Type")
 employeeDictionary = {"name" : "John", "age" : 36} # Dict
 print(f">>> Type of employeeDictionary is {type(employeeDictionary)}")
-print('employee dictionary: ', employeeDictionary);
+print('employee dictionary: ', employeeDictionary)
 config = {True : "dummyValue", 1 : "actualConfigValue"} # Dict key will same True but value will be overriden.
-print('config dictionary: ', config);
+print('config dictionary: ', config)
 
 print("\n>>> Boolean Type")
 eligibleToVote = True # Boolean
 print(f">>> Type of eligibleToVote is {type(eligibleToVote)}")
-print('eligibleToVote: ', eligibleToVote);
+print('eligibleToVote: ', eligibleToVote)
 
 # TODO
 # Add Bytes
