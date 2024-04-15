97c9f7d84e31125b5626a3611dcd3042a1847189
Venkat.R <ramsunvtech@gmail.com>
2024-04-15 20:40:32 +0800

    semi colon removal

--- basics/a_basics.py
+++ basics/a_basics.py
@@ -4,7 +4,7 @@ print('\n # Basic Example')
 # Print Statement without parenthesis.
 print('\n >>>> Print Statement without parenthesis throws SyntaxError')
 # SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
-# print "Concatenation with variable";
+# print "Concatenation with variable"
 
 # Print Statement
 print('\n >>>> Print Statement ')
@@ -12,6 +12,8 @@ print('hello venkat, welcome to python')
 
 # Print Statement support comma separated arguments.
 print('\n >>>> Print Statement support n number of comma separated arguments.')
+#  Maximum number of parameters you can pass to a function like print()
+#  It is primarily limited by the amount of memory available on your system.
 print('hello venkat,', 'welcome to ', 'python')
 
 # Concatenation
@@ -20,7 +22,7 @@ print('hello venkat, welcome to python' + '3')
 
 # Concatenation with int variable
 print('\n >>>> Concatenation with int variable and string throws TypeError')
-version = 3;
+version = 3
 # Your will get TypeError: unsupported operand type(s) for +: 'int' and 'str'
 # print(version + 'hello venkat, welcome to python')
 # Your will get TypeError: can only concatenate str (not "int") to str.
@@ -31,25 +33,25 @@ print('hello venkat, welcome to python' + str(version))
 
 # Concatenation with string variable
 print('\n >>>> Concatenation with string variable')
-version = '3';
+version = '3'
 print('hello venkat, welcome to python' + version)
 
 # Concatenation with string variable wrapped in String literal.
 print('\n >>>> String variable wrapped in String literal.')
-version = '3';
+version = '3'
 print(f"hello venkat, welcome to python{version}")
 
 
 # Simple Hello World Function throws IndentationError when function body statement with zero indentation.
-print('\n >>>> Simple Hello World Function throws IndentationError when function body statement with zero indentation.');
-print(' >>>> Fix is 4 tabs after def statement');
+print('\n >>>> Simple Hello World Function throws IndentationError when function body statement with zero indentation.')
+print(' >>>> Fix is 4 tabs after def statement')
 def helloWorld():
 # You will get IndentationError: expected an indented block after function definition on line 47
 # In Python, the execution order is typically from top to bottom, line by line.
 # Python first compiles your code into bytecode before it executes it. During the compilation phase, it checks for syntax errors. 
 # if there is an IndentationError during compilation anywhere in your code, Python will not execute any part of the script because the error is caught during the compilation phase.
 # print('\nHello World from helloWorld()')
-    print('\nHello World from helloWorld()')
+  print('\nHello World from helloWorld()')
 
 # Calling the function
 helloWorld()
