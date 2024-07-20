5a80d6760e5b3082bf17d1e115e50fdae69cd76e
praveenmahesh06 <praveenmahesh06@gmail.com>
2024-07-20 13:55:38 +0530

    remove module not found error

--- data_types/binary/bytearray/basics.py
+++ data_types/binary/bytearray/basics.py
@@ -1,7 +1,12 @@
 # python module.py data_types/binary/bytearray/basics.py
 # python -m data_types.binary.bytearray data_types/binary/bytearray/basics.py
+import sys
+import os
 
-import data_types.commonUtils as utils
+# Add the parent directory of 'data_types' to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+
+from data_types import commonUtils as utils
 
 utils.print_h1('Byte Array Basics')
 
