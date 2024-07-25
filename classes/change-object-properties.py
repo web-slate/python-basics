36284e524a274ca8c7840cd6ce235930dcf5844a
Sruthi S <sruthisudharsanan123@gmail.com>
2024-07-25 13:22:54 +0530

    Update change-object-properties.py

--- classes/change-object-properties.py
+++ classes/change-object-properties.py
@@ -12,14 +12,14 @@ bus = sc.Bus()
 h1(f"Bus length : {bus.length}")
 bus.length = 10
 bus.get_length = get_length1
-h5(f"Bus length : {bus.length}")
-h6(f"bus length is : {bus.get_length(bus)}")
+h2(f"Bus length : {bus.length}")
+h3(f"bus length is : {bus.get_length(bus)}")
 bus.additional_param = 9
 
 # we can assign and change the properties from out side but it is a bad design
 
 
-h5(f" bus additional param : {bus.additional_param}")
+h4(f" bus additional param : {bus.additional_param}")
 
 apple = sc.Fruit()
 
